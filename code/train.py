887d027f333a0c7e4759c7096c4f241b6c5c3b86
skatchoww <bntngfkr@gmail.com>
2026-03-13 16:46:34 +0700

    fix experiment 13: balik ke 30 sec

--- code/train.py
+++ code/train.py
@@ -273,7 +273,7 @@ def train(fold=0):  # ✅ TAMBAHKAN parameter fold
 
         wandb.init(
             project=Config.WANDB_PROJECT,
-            name=f"Mamba_Fold_{current_fold}_2Class_10secWindows",
+            name=f"Mamba_Fold_{current_fold}_2Class_30secWindows",
             config=clean_config,  
             reinit=True  
         )
