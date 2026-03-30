0bd1c0451a2e85f8015819ca28cab7cc0993780f
skatchoww <bntngfkr@gmail.com>
2026-03-30 20:08:48 +0700

    label smoothing

--- code/train.py
+++ code/train.py
@@ -371,7 +371,7 @@ def train(fold=0):  # ✅ TAMBAHKAN parameter fold
     )
     
     # Focal Loss
-    criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
+    criterion = torch.nn.CrossEntropyLoss(weight=class_weights, label_smoothing=0.1)
     
     # TAMBAHKAN Learning Rate Scheduler 
     scheduler = None
